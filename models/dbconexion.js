5b90f78025459f433cae81852f37b7422b2c1227
Daniel Pat <danielpat6@gmail.com>
2019-07-25 13:55:56 -0500

    db

--- models/dbconexion.js
+++ models/dbconexion.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/personas',{useNewUrlParser:true}).then(_=>console.log('Base de datos conectada'));
+mongoose.connect('mongodb://personascrud:Ba43P7J~8?sU@den1.mongo1.gear.host:27001/personascrud',{useNewUrlParser:true}).then(_=>console.log('Base de datos conectada'));
 
 module.exports= mongoose;
